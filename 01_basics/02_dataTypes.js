81f7df8b1fcfe3b5f4aa422dcfdbb9033a123fb2
ADITYAAGAR1221 <128697828+ADITYAAGAR1221@users.noreply.github.com>
2024-02-23 15:46:45 +0000

    datatype

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,35 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3 )  // we are using nodejs, not browser
+
+console.log(3 
+    + 
+     3) // this is correct but code readability should be high so we should not write like this 
+
+console.log("Aditya");
+
+
+let name = "Aditya"
+let age = 21
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => uniqueṇ
+
+// object
+
+console.log(typeof "Aditya");
+console.log(typeof 21);
+console.log(typeof age);
+console.log(typeof isLoggedIn);
+console.log(typeof null); // object
+console.log(typeof undefined); //undefined
+
+
+
